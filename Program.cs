0a47db67809824254e06adbdcb00755a9ae41471
uit2712 <phamquangvi456@gmail.com>
2024-09-01 19:16:32 +0000

    update docker

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Core.EnglishContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EnglishContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
