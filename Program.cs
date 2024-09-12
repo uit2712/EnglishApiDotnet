908efbf4455ca9a4b846eaacf87eca5f71af5c5d
uit2712 <phamquangvi456@gmail.com>
2024-09-12 00:32:46 +0000

    seed data

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Core.Features.FileReader.DependencyInjection;
 using Core.Features.Group.DependencyInjection;
 using Core.Features.Topic.DependencyInjection;
 using Core.Features.Vocabulary.DependencyInjection;
+using Core.Seed;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,6 +44,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        SeedData.Initialize(services);
+    }
 }
 
 app.UseHttpsRedirection();
--- docker/clear-logs.sh
+++ docker/clear-logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "" > $(docker inspect --format='{{.LogPath}}' db) && \
+echo "" > $(docker inspect --format='{{.LogPath}}' api) && \
 echo "" > $(docker inspect --format='{{.LogPath}}' adminer)
